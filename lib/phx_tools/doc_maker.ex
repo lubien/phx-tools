0eca24f3be1f5e89affac5d66d3bb06f1b407a8a
lubien <lubien1996@gmail.com>
2023-06-12 20:21:23 -0300

    Simplify stuff

--- lib/phx_tools/doc_maker.ex
+++ lib/phx_tools/doc_maker.ex
@@ -12,8 +12,14 @@ defmodule PhxTools.DocMaker do
 
   defp find_assigns_outside_attrs(str) do
     str
-    |> comp()
-    |> find_assigns()
+    |> EEx.compile_string(engine: EEx.SmartEngine)
+    |> Macro.prewalk([], fn
+      {{:., _, _}, _, [{:var!, _, _}, assign_name]} = x, acc ->
+        {x, [assign_name | acc]}
+
+      x, acc ->
+        {x, acc}
+    end)
     |> elem(1)
   end
 
@@ -23,8 +29,6 @@ defmodule PhxTools.DocMaker do
     |> Enum.flat_map(& find_assigns_outside_attrs("<%= #{&1} %>"))
   end
 
-  @eex_expr [:start_expr, :expr, :end_expr, :middle_expr]
-
   defp tokenize(source) do
     {:ok, eex_nodes} = EEx.tokenize(source)
     {tokens, cont} = Enum.reduce(eex_nodes, {[], :text}, &do_tokenize(&1, &2, source))
@@ -32,24 +36,14 @@ defmodule PhxTools.DocMaker do
     |> find_eex_expressions([])
   end
 
-  defp do_tokenize({:text, text, meta}, {tokens, cont}, source) do
+  defp do_tokenize({:text, text, _meta}, {tokens, cont}, contents) do
     text = List.to_string(text)
-    meta = [line: meta.line, column: meta.column]
-    state = Tokenizer.init(0, "nofile", source, Phoenix.LiveView.HTMLEngine)
+    meta = []
+    state = Tokenizer.init(0, "nofile", contents, Phoenix.LiveView.HTMLEngine)
     Tokenizer.tokenize(text, meta, tokens, cont, state)
   end
 
-  defp do_tokenize({:comment, text, meta}, {tokens, cont}, _contents) do
-    {[{:eex_comment, List.to_string(text), meta} | tokens], cont}
-  end
-
-  defp do_tokenize({type, opt, expr, %{column: column, line: line}}, {tokens, cont}, _contents)
-       when type in @eex_expr do
-    meta = %{opt: opt, line: line, column: column}
-    {[{:eex, type, expr |> List.to_string() |> String.trim(), meta} | tokens], cont}
-  end
-
-  defp do_tokenize({:eof, _data}, {tokens, cont}, _contents) do
+  defp do_tokenize(_anything, {tokens, cont}, _contents) do
     {tokens, cont}
   end
 
@@ -65,27 +59,5 @@ defmodule PhxTools.DocMaker do
   end
   defp find_eex_expressions([{_type, _str, _meta} | rest], acc) do
     find_eex_expressions(rest, acc)
-  end
-
-  # defp eval(string, assigns \\ %{}, binding \\ []) do
-  #   # EEx.eval_string(string, [assigns: assigns] ++ binding, file: __ENV__.file, engine: Engine)
-  #   EEx.eval_string(string, [assigns: assigns] ++ binding,
-  #     file: __ENV__.file,
-  #     engine: EEx.SmartEngine
-  #   )
-  # end
-
-  defp comp(string) do
-    EEx.compile_string(string, engine: EEx.SmartEngine)
-  end
-
-  defp find_assigns(ast) do
-    Macro.prewalk(ast, [], fn
-      {{:., _, _}, _, [{:var!, _, _}, assign_name]} = x, acc ->
-        {x, [assign_name | acc]}
-
-      x, acc ->
-        {x, acc}
-    end)
   end
 end
